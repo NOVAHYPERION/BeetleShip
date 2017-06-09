2e7ad79d3999ea6c84b51aa39e5fc765160ee45b
Leonardo Lamboglia <ll3087@nyu.edu>
2017-06-09 15:10:29 -0400

    Add files via upload
    Clickability of blocks

---
+++ Clickablility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickablility : MonoBehaviour {
+    public TileMap Map;
+	public float LocX;
+    public float LocY;
+    
+    void OnMouseUp()
+    {
+        Debug.Log("FUCK");
+        Debug.Log(LocX);
+        Debug.Log(LocY);
+        Map.MoveSelf(LocX, LocY);
+    }
+
+}
